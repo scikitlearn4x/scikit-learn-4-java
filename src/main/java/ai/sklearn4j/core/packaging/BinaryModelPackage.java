7aabb2bc7799c4bd55b95a96b8ccb429d61c54f9
Mohammad Ali Yektaie <yektaie112358@gmail.com>
2022-05-11 23:27:04 -0700

    Project created

---
+++ src/main/java/ai/sklearn4j/core/packaging/BinaryModelPackage.java
@@ -0,0 +1,19 @@
+package ai.sklearn4j.core.packaging;
+
+public class BinaryModelPackage {
+    private static final int ELEMENT_TYPE_BYTE = 0x01;
+    private static final int ELEMENT_TYPE_SHORT = 0x02;
+    private static final int ELEMENT_TYPE_INT = 0x04;
+    private static final int ELEMENT_TYPE_LONG = 0x08;
+    private static final int ELEMENT_TYPE_UNSIGNED_BYTE = 0x11;
+    private static final int ELEMENT_TYPE_UNSIGNED_SHORT = 0x12;
+    private static final int ELEMENT_TYPE_UNSIGNED_INT = 0x14;
+    private static final int ELEMENT_TYPE_UNSIGNED_LONG = 0x18;
+    private static final int ELEMENT_TYPE_FLOAT = 0x20;
+    private static final int ELEMENT_TYPE_DOUBLE = 0x21;
+    private static final int ELEMENT_TYPE_STRING = 0x30;
+    private static final int ELEMENT_TYPE_LIST = 0x40;
+    private static final int ELEMENT_TYPE_DICTIONARY = 0x41;
+
+
+}
